5a69c5742d3755ae6f00c783a08d118d6edd59a0
Emilio Gallo <emilio.gallo17@gmail.com>
2024-07-01 21:53:32 +0200

    feat: created and linked placeholder pages - ContactsPage & HomePage

---
+++ src/components/ContactsPage.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+   name: "ContactsPage"
+}
+</script>
+
+<template>
+
+   <div class="container d-flex flex-column align-items-center">
+
+      <h1 class="text-primary my-3">Contacts Page</h1>
+
+   </div>
+
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+   name: "HomePage"
+}
+</script>
+
+<template>
+
+   <div class="container d-flex flex-column align-items-center">
+
+      <h1 class="text-primary my-3">Home Page</h1>
+
+   </div>
+
+</template>
+
+<style scoped lang="scss"></style>
