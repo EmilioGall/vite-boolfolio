5b89f7daeef091cdcec9db6e19a3b002a60840ae
Emilio Gallo <emilio.gallo17@gmail.com>
2024-07-01 21:05:32 +0200

    feat: added scaffolding of Header NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 
 export default {
   components: {
 
     AppMain,
+    AppHeader,
 
   },
 
@@ -12,6 +14,8 @@ export default {
 </script>
 
 <template>
+
+  <AppHeader />
 
   <AppMain />
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './scss/general.scss'
+import "bootstrap"
 import App from './App.vue'
 
 createApp(App).mount('#app')
